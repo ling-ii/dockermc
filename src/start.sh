8dea7fcdfab0574a709bc605c6e6d3dbbd5999f5
michael <michael.ling128@gmail.com>
2024-09-05 19:02:17 +1000

    Fixed start.sh

--- src/start.sh
+++ src/start.sh
@@ -2,4 +2,4 @@
 
 echo 'Starting server...' &&
 echo &&
-java -Xmx2G -jar fabric-server-mc.1.20.6-loader.0.15.10-launcher.1.0.1.jar nogui
+java -Xmx2G -jar fabric-server-mc.1.21.1-loader.0.16.4-launcher.1.0.1.jar nogui
